92feb42a093873cab372de6f50e75094a5c485cf
Asylbek <asola@list.ru>
2024-06-08 01:58:32 +0500

    отмечаем мины

--- main.py
+++ main.py
@@ -46,8 +46,23 @@ class MineSweeper:
             for j in range(MineSweeper.COLUMNS + 2):
                 btn = MyButton(MineSweeper.window, x=i, y=j)
                 btn.config(command=lambda button=btn: self.click(button))
+                btn.bind("<Button-3>", self.right_click)
                 temp.append(btn)
             self.buttons.append(temp)
+
+    def right_click(self, event):
+        if MineSweeper.IS_GAME_OVER:
+            return
+        cur_btn = event.widget
+        if cur_btn['state'] == 'normal':
+            cur_btn['state'] = 'disabled'
+            cur_btn['text'] = '🚩'
+            cur_btn['disabledforeground'] = 'red'
+        elif cur_btn['text'] == '🚩':
+            cur_btn['text'] = ''
+            cur_btn['state'] = 'normal'
+
+
 
     def click(self, clicked_button: MyButton):
 
